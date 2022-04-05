b9ae4ad535a7804084c422c5f81500fbd6728b89
Carlos <chenriquep6@gmail.com>
2022-04-04 21:24:45 -0300

    todos os arquivos

--- src/main/java/cavalo.java
+++ src/main/java/cavalo.java
@@ -7,27 +7,39 @@ public class cavalo {
         double b;
         double c;
         double d;
-//        System.out.println("quantos cavalos você tem?");
-//        b= multi.nextInt();
-//        System.out.println("você precisara desse tanto de ferraduras");
-//        a=4*b;
-//        System.out.println(a);
+        String e;
+        System.out.println("quantos cavalos você tem?");
+        b= multi.nextInt();
+        System.out.println("você precisara desse tanto de ferraduras");
+        a=4*b;
+        System.out.println(a);
 
 
 
-//        a=0.12;
-//        b=1.50;
-//        System.out.println("quantos pães você vendeu?");
-//        c= multi.nextInt();
-//        System.out.println(a*c);
-//        System.out.println("quantas broas você vendeu?");
-//        c= multi.nextInt();
-//        System.out.println(b*c);
-//        System.out.println("no total você vendeu:");
-//        d= a*c+b*c;
-//        System.out.println(d);
-//        System.out.println("você deve separar isso para sua poupança");
-//        System.out.println(d/10);
+        a=0.12;
+        b=1.50;
+        System.out.println("quantos pães você vendeu?");
+        c= multi.nextInt();
+        System.out.println("você lucrou "+a*c);
+        System.out.println("quantas broas você vendeu?");
+        c= multi.nextInt();
+        System.out.println("você lucrou "+b*c);
+        System.out.println("no total você vendeu:");
+        d= a*c+b*c;
+        System.out.println(d);
+        System.out.println("você deve separar isso para sua poupança");
+        System.out.println(d/10);
+
+
+        a=365;
+        System.out.println("qual o seu nome?");
+        e= multi.next();
+        System.out.println("qual a sua idade");
+        c= multi.nextInt();
+        d=a*c;
+        System.out.println(e+ " você viveu " +d+ " dias ");
+
+
 
 
 
