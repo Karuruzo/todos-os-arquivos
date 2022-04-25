252e9f5572d931de7377033420dddbe5f78f7dc2
Carlos <chenriquep6@gmail.com>
2022-04-25 13:02:42 -0300

    todos os arquivos

---
+++ src/main/java/testizindobiel.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class testizindobiel {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        double a;
+        double b;
+        double c;
+        double d;
+    }
+}
